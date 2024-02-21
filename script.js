2344fe1e93f25b10bf1f664c6b90548346f5b2ae
Bereket Getachew <146061009+Beki78@users.noreply.github.com>
2024-02-21 21:21:12 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    const sideBar = document.querySelector(".mobileNavbar");
+    sideBar.style.display = "flex"
+}
+function closeMenu() {
+  const sideBar = document.querySelector(".mobileNavbar");
+  sideBar.style.display = "none";
+}
+
